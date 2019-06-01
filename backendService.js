0927df037dbe98a517395ca7248015670d4c4246
Michael Odumosu <michaelodumosu57@gmail.com>
2019-06-01 18:14:00 -0400

    learned about parallel catergories allowing you to visualize data about common possive trait characteristics

--- backendService.js
+++ backendService.js
@@ -6,13 +6,50 @@ const path = require('path')
 const fs = require('fs');
 const compression = require('compression')
 const cors = require('cors')
+const linspace = require('linspace')
 //middleware
+global.ultraObject = require('./ultraObject.js')
 const backend = express.Router()
 //
 
 app.use(   '/backend',backend   )
 backend.get(   '/index',function(req,res){
-    res.sendFile(__dirname + '/charts.html')
+    res.sendFile(__dirname + '/statisticalCharts.html')
+})
+backend.post(   '/linspace',function(req,res){
+    
+	ultraObject.reqBody({
+		stream:req,
+		fn:function(dev_obj){
+		},
+		keep:'true',
+		finish:function(dev_obj){
+		    dev_obj.stream.body = JSON.parse(dev_obj.stream.body)
+		    res.send(   linspace(   dev_obj.stream.body.a,dev_obj.stream.body.b,dev_obj.stream.body.c   )   )
+		}
+		
+	})
+
+    
+    // res.send(JSON.stringify(linspace))
+})
+
+backend.post(   '/Treemap',function(req,res){
+    
+	ultraObject.reqBody({
+		stream:req,
+		fn:function(dev_obj){
+		},
+		keep:'true',
+		finish:function(dev_obj){
+		    dev_obj.stream.body = JSON.parse(dev_obj.stream.body)
+		    res.send(   'where is Treemaps'   )
+		}
+		
+	})
+
+    
+    // res.send(JSON.stringify(linspace))
 })
 backend.get(   '/plotly',function(req,res){
     res.sendFile(__dirname +'/plotly-latest.min.js')
